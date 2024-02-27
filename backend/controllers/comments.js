f812233616fcbd14f47216252494508676476cb6
Jahdai <jahdai.kilkenny@gmail.com>
2024-02-27 13:56:30 -0500

    comments have jwauth, backend.js updated to include all auth and non auth requests...need to move on to creating plant database and figure out how to pull info from api and add to db

--- backend/controllers/comments.js
+++ backend/controllers/comments.js
@@ -1,8 +1,35 @@
-const jwt = require('jwt-simple')
+const jwt = require("jwt-simple");
 const express = require("express");
 const router = express.Router();
 
 const db = require("../models");
+
+const config = require("../../jwt.config.js");
+const user = require("../models/user.js");
+
+/* Middleware that checks if a JWT sent from the client is valid.
+   Used for all routes that require authorization
+--------------------------------------------------------------- */
+const authMiddleware = (req, res, next) => {
+  // Check if the 'Authorization' header is present and has the token
+  const token = req.headers.authorization;
+  if (token) {
+    try {
+      // Decode the token using the secret key and add the decoded payload to the request object
+      const decodedToken = jwt.decode(token, config.jwtSecret);
+      req.user = decodedToken;
+      next();
+    } catch (err) {
+      // Return an error if the token is invalid
+      res.status(401).json({ message: "Invalid token" });
+    }
+  } else {
+    // Return an error if the 'Authorization' header is missing or has the wrong format
+    res
+      .status(401)
+      .json({ message: "Missing or invalid Authorization header" });
+  }
+};
 
 //Routes:
 //get
@@ -12,20 +39,36 @@ router.get("/:plantId", function (req, res) {
   );
 });
 //create
-router.post("/", (req, res) => {
-  db.Comment.create(req.body).then((comment) => res.json(comment));
+router.post("/", authMiddleware, (req, res) => {
+  db.Comment.create({
+    ...req.body,
+    userId: req.user.id,
+  }).then((comment) => res.json(comment));
 });
 //update
-router.put("/:id", (req, res) => {
-  db.Comment.findByIdAndUpdate(req.params.id, req.body, { new: true }).then(
-    (comment) => res.json(comment)
-  );
+router.put("/:id", authMiddleware, async (req, res) => {
+  const userComment = await db.Comment.findById(req.params.id);
+  if (userComment.userId == req.user.id) {
+    const newComment = await db.Comment.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+    res.json(newComment);
+  } else {
+    res.status(401).json({ message: "Invalid user or token" });
+  }
 });
+
 //destroy
-router.delete("/:id", (req, res) => {
-  db.Comment.findByIdAndDelete(req.params.id).then(() =>
-    res.json({ deletedCommentId: req.params.id })
-  );
+router.delete("/:id", authMiddleware, async (req, res) => {
+  const userComment = await db.Comment.findById(req.params.id);
+  if (userComment.userId == req.user.id) {
+    const deletedComment = await db.Comment.findByIdAndDelete(req.params.id);
+    res.send("You deleted comment " + deletedComment._id);
+  } else {
+    res.status(401).json({ message: "Invalid user or token" });
+  }
 });
 
 module.exports = router;
--- frontend/utils/backend.js
+++ frontend/utils/backend.js
@@ -5,8 +5,33 @@ export async function getComments(plantId){
     const { data } = await axios.get(`/api/comments/${plantId}`)
     return data
 }
-
 //Authentication Requests
+export async function signUp(user) {
+    const { data } = await axios.post('/api/users/signup', user)
+    return data
+}
+
+export async function logIn(user) {
+    const { data } = await axios.post('/api/users/login', user)
+    return data
+}
 
 
 //Authorization requests
+export async function postComment(comment) {
+    const authHeader = { headers: { 'Authorization': localStorage.getItem('userToken') } }
+    const { data } = await axios.post('/api/comments', comment, authHeader)
+    return data
+}
+
+export async function updateComment(comment, id) {
+    const authHeader = { headers: { 'Authorization': localStorage.getItem('userToken') } }
+    const { data } = await axios.put(`/api/comments/${id}`, comment, authHeader)
+    return data
+}
+
+export async function deleteComment(id) {
+    const authHeader = { headers: { 'Authorization': localStorage.getItem('userToken') } }
+    const { data } = await axios.delete(`/api/comments/${id}`, authHeader)
+    return data
+}
