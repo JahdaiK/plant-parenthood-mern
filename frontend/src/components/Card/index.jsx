914691e50f61b37293420885b1b7d4df54acddb1
Jahdai <jahdai.kilkenny@gmail.com>
2024-02-28 12:33:37 -0500

    Cards are clickable and brought to a details page that renders plant information

--- frontend/src/components/Card/index.jsx
+++ frontend/src/components/Card/index.jsx
@@ -1,9 +1,12 @@
+import { Link } from "react-router-dom";
 export default function Card({ plant }) {
   return (
     <>
-      <div className="border-solid border-2 border-sky-500 rounded overflow-hidden shadow-lg flex flex-col mx-3">
-       {plant.common_name}
-      </div>
+      <Link to={"details/" + plant.id}>
+        <div className="border-solid border-2 border-sky-500 rounded overflow-hidden shadow-lg flex flex-col mx-3">
+          {plant.common_name}
+        </div>
+      </Link>
     </>
   );
 }
--- frontend/src/components/DetailsPage/index.jsx
+++ frontend/src/components/DetailsPage/index.jsx
@@ -1,23 +1,21 @@
-import {useParams} from 'react-router-dom';
-import {useEffect, useState } from 'react';
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
 
-export default function DetailsPage(){
-    const {id} = useParams();
-    const [plantDetails, setPlantDetails] = useState(null);
+export default function DetailsPage() {
+  const { id } = useParams();
+  const [plantDetails, setPlantDetails] = useState(null);
 
-    useEffect(()=>{
-        const fetchPlantDetails = async () => {
-            const response = await fetch(
-                `https://perenual.com/api/species/details/${id}?key=sk-Tsec65dca9e466d594369`
-            );
-            const data = await response.json();
-            setPlantDetails(data);
-        }
-        if(plantDetails?.id !== id || !plantDetails) fetchPlantDetails();
-    },[])
-    return(
-        <>
-            <p>You've reached details</p>
-        </>
-    )
+  useEffect(() => {
+    const fetchPlantDetails = async () => {
+      const response = await fetch(
+        `https://perenual.com/api/species/details/${id}?key=sk-Tsec65dca9e466d594369`
+      );
+      const data = await response.json();
+      setPlantDetails(data);
+    };
+    fetchPlantDetails();
+  }, []);
+  return (
+    <>{plantDetails ? <p>{plantDetails.common_name}</p> : <p>Loading</p>}</>
+  );
 }
